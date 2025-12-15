c526605b0dbbf7b702e5ca7f895d537ce1d58a87
MightyPhoonProductions <mightyphoonproductions@gmail.com>
2025-12-15 11:59:07 +0000

    wtf

--- Chrismas_Game_Jam/Assets/Scripts/Player_Movement.cs
+++ Chrismas_Game_Jam/Assets/Scripts/Player_Movement.cs
@@ -1,16 +1,79 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Player_Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+    [Header("Movement")]
+    public float moveSpeed = 6f;
+    public float jumpHeight = 1.6f;
+    public float gravity = -9.81f;
+
+    [Header("Mouse Look")]
+    public float mouseSensitivityX = 100f;
+    public float mouseSensitivityY = 100f;
+    public Transform cameraTransform;
+    public float minY = -80f;
+    public float maxY = 80f;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private float xRotation = 0f;
+    private bool isGrounded;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleMouseLook();
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        isGrounded = Physics.CheckSphere(
+            groundCheck.position,
+            groundDistance,
+            groundMask
+        );
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityX * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, minY, maxY);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
     }
 }
